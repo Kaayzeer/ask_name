6eb2910cea26818c7e876743553cf86d09b3c7bd
nikolas <nikolas.rosinelli@atudent.kyh.se>
2021-09-19 22:07:20 +0200

    some styling added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let popup = document.querySelector('#popupId')
-let name = document.querySelector('#nameId')
-let input = document.querySelector('#inputId')
+
 let button = document.querySelector('#inputBtn')
-let body = document.querySelector('body')
+
+let imgDiv = document.querySelector('#imgDiv')
 
 let savedNames = []
 
@@ -12,9 +12,7 @@ const setStorage = (name) => {
 
 const getStorage = () => {                          // function that get savedNames array from localStorage 
     let ls = JSON.parse(localStorage.getItem('name'))
-    console.log(ls)
-
-    if(!ls){                                        // if there is something there, if not it returns an empty array
+    if(!ls){                                        // if there isn´t something there, Hejpa, and Dejpa will be saved at Local Storage
       savedNames.push('Hejpa', 'Dejpa')
       setStorage(savedNames)
     }
@@ -27,11 +25,11 @@ document.onload =  savedNames = getStorage()
 const createChild = (name) => { 
 
     let newH1 = document.createElement("H1")
-    let text = !savedNames.includes(name) ? document.createTextNode(`Välkommen ${name}`) : document.createTextNode(`Välkommen tillbaka ${name}`) 
+    let text = !savedNames.includes(name) ? document.createTextNode(`Welcome ${name}`) : document.createTextNode(`Welcome back ${name}`) 
     
     newH1.appendChild(text)
     newH1.classList.add("popH1")
-    body.prepend(newH1) 
+    imgDiv.appendChild(newH1);
 
      setTimeout(() => {
         newH1.remove()
@@ -39,6 +37,7 @@ const createChild = (name) => {
 }
 
 button.addEventListener("click", (e) => {           // eventListener that targets the button
+  e.preventDefault()
   let inputName = e.target.form[0].value;           // the input fields value
             
   if (inputName) {                                    //if the value of the input field is truthy 
@@ -57,8 +56,6 @@ button.addEventListener("click", (e) => {           // eventListener that target
 
 
 
-
-//Från start på ny dator/webbläsare redan är besökt av besökarna ”Hejpa” och ”Dejpa”. 
 
 
 
